51c379df86cb177deddea34a57fb965731e7f355
Rob Speer <rspeer@mit.edu>
2011-03-16 16:56:11 -0400

    implemented what should be a simplenlp reader for English

--- luminoso/text_readers.py
+++ luminoso/text_readers.py
@@ -5,70 +5,107 @@ from simplenlp import get_nl
 # - Handle situations that aren't just about discovering terms. (Negations,
 #   different strengths of terms, etc.) Change the API to make this possible.
 
+DOCUMENT = u'*Document*'
 
 class TextReader(object):
     """
     An abstract class showing the interface that TextReaders must implement.
     """
-    def tokenize(self, text):
+    def extract_connections(self, text):
         """
-        Split a text into words.
-        """
-        raise NotImplementedError
-
-    def extract_terms_by_sentence(self, text):
-        """
-        Extract all terms from a text (including multi-word terms), returning
-        a list of lists with one list per sentence.
+        Outputs a list of tuples, containing two terms and the strength with
+        which they are connected. These connections are symmetrical, so there
+        is no need to output them once in each direction.
+        
+        It should also output a special connection at least once per term,
+        with the first term being the special symbol called DOCUMENT.
         """
         raise NotImplementedError
-    
-    def extract_terms(self, text):
-        """
-        Extract all terms from a text (including multi-word terms), ignoring
-        sentence boundaries if they are present.
-
-        The default implementation of this will simply flatten the results
-        of extract_terms_by_sentence.
-        """
-        return [word for sentence in self.extract_terms_by_sentence(text)
-                     for word in sentence]
 
 class QuickEnglishTextReader(TextReader):
     """
     Uses simplenlp for handling English text.
+
+    The model of connections between words is one that decreases geometrically
+    until a cutoff value. The default values allow for associations between
+    words that are up to 20 tokens apart.
+
+    It also includes negative contexts. When one term in a pair appears in a
+    negative context, the sign of the connection is flipped. The sign of the
+    negated term's connection to the document is also flipped.
     """
-    # punctuation that marks a high-level division in a text; tokens that begin
-    # with one of these such as "..." or "?!" count as well
-    SENTENCE_PUNCT = [u'.', u'?', u'!', u':', u'…']
+    # this punctuation symbol marks the strongest possible division in a text,
+    # so that no word associations can occur across it. We intend for this
+    # to be inserted manually. For example, if two reviews from different
+    # people got concatenated, write "text of first review // text of second".
+    HARD_PUNCT = [u'//']
 
-    # punctuation characters that should end a negative context
-    PUNCT = SENTENCE_PUNCT + [u'-', u':', u';', u'—']
+    # punctuation characters that end a negative context; tokens that begin
+    # with one of these such as "..." or "?!" count as well.
+    PUNCT = [u'.', u'?', u'!', u':', u'…', u'-', u'—']
 
-    # punctuation that have to appear as the entire token to count
-    # (so we don't accidentally treat "'s" as punctuation)
-    PUNCT_TOKENS = [u"''", u"``", u"'", u"`"]
+    # punctuation that has to appear as the entire token to count
+    # (so we don't accidentally treat "'s" as punctuation, for example)
+    PUNCT_TOKENS = [u"''", u"``", u"'", u"`", u":", u";"]
 
     # words that flips the context from positive to negative
     NEGATIONS = [u'no', u'not', u'never', u'stop', u'lack',
                  u"n't", u'without']
 
-    def __init__(self):
+    def __init__(self, distance_weight=0.9, negation_weight=-0.5, cutoff=0.1):
         self.nl = get_nl('en')
+        self.negation_weight = negation_weight
+        self.distance_weight = distance_weight
+        self.cutoff = cutoff
 
     def tokenize(self, text):
         return self.nl.tokenize(text).split()
     
-    def extract_terms_by_sentence(self, text):
-        sentences = []
+    def _attenuate(self, memory):
+        for i in reversed(xrange(len(memory))):
+            memory[i][1] *= self.distance_weight
+            if memory[i][1] < self.cutoff:
+                del memory[i]
+
+    def extract_connections(self, text):
+        c = self.__class__
+        
+        # convenient shorthand
         tokens = self.tokenize(text)
-        current_sentence = []
+        
+        weight = 1.0
+        memory = []
+        prev_token = None
+
         for token in tokens:
-            if token: # protect against empty strings if they show up somehow
-                if token[0] in PUNCT or token in FIDDLY_PUNCT:
+            if token: # protect against getting empty tokens somehow
+                self._attenuate(memory)
+                if token in c.HARD_PUNCT:
+                    memory = []
+                    weight = 1.0
+                    prev_token = None
+                elif token in c.PUNCT_TOKENS or token[0] in c.PUNCT:
+                    weight = 1.0
+                    prev_token = None
+                elif token in c.NEGATIONS:
+                    weight *= self.negation_weight
+                    prev_token = None
+                else:
+                    # this is an ordinary token, not a negation or punctuation
+                    active_terms = [token]
+                    if prev_token is not None:
+                        bigram = prev_token + u' ' + token
+                        active.append(bigram)
+                    for term in active_terms:
+                        yield (weight, DOCUMENT, term)
+                        memory.append([term, weight])
 
-
-    # TODO: finish
+                    for term in active_terms:
+                        for prev, prev_weight in memory:
+                            # Currently, this will associate each term with
+                            # itself, in addition to previous terms.
+                            # Is this the right way to do that?
+                            yield (weight*prev_weight, prev, term)
 
 ## Other things to include eventually:
 # class JapaneseTextReader(TextReader):
