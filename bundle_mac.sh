a6148af8c1d05f20fe858cdfc2ee876c627f2e52
Rob Speer <rspeer@mit.edu>
2011-03-17 13:50:42 -0400

    Burning everything down except the new code. This helps to clear my head.

--- bundle_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p dist/mac/Luminoso
-cp -r luminoso env AnalogySpace ThaiFoodStudy icons README Luminoso.app dist/mac/Luminoso/
--- luminoso/scripts/update_conceptnet.py
+++
@@ -1,9 +0,0 @@
-from luminoso.whereami import *
-from csc.conceptnet.analogyspace import conceptnet_2d_from_db
-import cPickle as pickle
-
-picklefile = open(package_dir+'/study_skel/Matrices/conceptnet.pickle', 'w')
-cnet = conceptnet_2d_from_db('en')
-pickle.dump(cnet, picklefile)
-picklefile.close()
-
